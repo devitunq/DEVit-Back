90fa87356458df2883e97c12ce82a347f58dc887
LM-Randomize <lcmurc@gmail.com>
2020-12-04 20:14:03 -0300

    Arreglo bug estrellas

--- src/main/kotlin/ar/edu/unq/devit/model/StarsSystem.kt
+++ src/main/kotlin/ar/edu/unq/devit/model/StarsSystem.kt
@@ -3,9 +3,8 @@ package ar.edu.unq.devit.model
 object StarsSystem {
 
     fun calculateStars(bestNumberOfMovements: Int, userMovements: Int): Int {
-        var starsWon = 0
+        var starsWon = 3
         when (bestNumberOfMovements  * 100 / userMovements) {
-            in 80..100 -> starsWon = 3
             in 70..80 -> starsWon = 2
             in 0..70 -> starsWon = 1
         }
